aa8a04f468fb08133d8df7c884089b030f7e00bf
DmitriyKryukovEkb <dmitriy_kryukov_ekb@mail.ru>
2023-10-14 11:32:50 +0500

    Version with function

--- task_0/Program.cs
+++ task_0/Program.cs
@@ -1,6 +1,14 @@
 ﻿// Напишите программу, которая на вход принимает трёхзначное число и на выходе показывает последнюю цифру этого числа
-System.Console.WriteLine("Введите трехзначное число: ");
-int inputNumber = Convert.ToInt32(Console.ReadLine());
+
+int ReadInt(string msg) // объявление функции
+{
+    System.Console.WriteLine(msg); // вывод на экран параметра msg функции
+    string inputString = Console.ReadLine(); // считывает строку из консоли
+    int number = int.Parse(inputString); // преобразует строку в число
+    return number; // возврат значения
+}
+
+int inputNumber = ReadInt("Введите трехзначное число: "); // вызов функции
 if (inputNumber > 99 & inputNumber < 1000)
 {
     System.Console.WriteLine($"Последняя цифра {inputNumber % 10}");
