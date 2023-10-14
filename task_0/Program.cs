c1721d4591a1bd5e292dab7cc729642407704514
DmitriyKryukovEkb <dmitriy_kryukov_ekb@mail.ru>
2023-10-14 11:23:29 +0500

    Version without function

--- task_0/Program.cs
+++ task_0/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает трёхзначное число и на выходе показывает последнюю цифру этого числа
+System.Console.WriteLine("Введите трехзначное число: ");
+int inputNumber = Convert.ToInt32(Console.ReadLine());
+if (inputNumber > 99 & inputNumber < 1000)
+{
+    System.Console.WriteLine($"Последняя цифра {inputNumber % 10}");
+}
+else
+{
+    System.Console.WriteLine("Число не трехзначное!");
+}
